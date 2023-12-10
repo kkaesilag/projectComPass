f7edde20349df14cb742d40aaaef3b84c6bb2841
kkaesilag <149465578+kkaesilag@users.noreply.github.com>
2023-12-10 16:20:51 +0900

    FAQ

---
+++ js/jscript_FAQ.js
@@ -0,0 +1,8 @@
+function toggleAnswer(answerId) {
+  var answer = document.getElementById(answerId);
+  if (answer.style.display === 'none') {
+    answer.style.display = 'block';
+  } else {
+    answer.style.display = 'none';
+  }
+}
