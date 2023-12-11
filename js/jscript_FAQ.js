323e7c30ccedc216fbdea046ddf3364f0e69ad9d
kkaesilag <149465578+kkaesilag@users.noreply.github.com>
2023-12-11 20:38:09 +0900

    FAQ 오류수정

--- js/jscript_FAQ.js
+++ js/jscript_FAQ.js
@@ -1,8 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // 페이지 로드 시 실행되며 모든 .answer 요소를 숨깁니다.
+  var answers = document.querySelectorAll('.answer');
+  answers.forEach(function(answer) {
+      answer.style.display = 'none';
+  });
+});
+
 function toggleAnswer(answerId) {
   var answer = document.getElementById(answerId);
-  if (answer.style.display === 'none') {
-    answer.style.display = 'block';
-  } else {
-    answer.style.display = 'none';
-  }
+  answer.style.display = answer.style.display === 'none' || answer.style.display === '' ? 'block' : 'none';
 }
